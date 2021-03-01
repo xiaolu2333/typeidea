f895bd736b5c4e89b3e95ae5fed7966205e29cfb
xiaolu2333 <2694977226@qq.com>
2021-03-01 17:10:51 +0800

    新增Post的后台管理功能

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+
 from .models import Post, Tag, Category
 import time
 
@@ -13,24 +16,65 @@ class CategoryAdmin(admin.ModelAdmin):
         if change:
             obj.owner = request.user
             localtime = time.asctime(time.localtime(time.time()))
-            category_old = self.model.objects.get(pk=obj.pk).name
+            try:
+                category_old = self.model.objects.get(name=obj.name).name
+            except:
+                category_old = "👨‍🍳初始信息"
             category_new = form.cleaned_data['name']
             f = open("/home/dfl/py-projects/web/django/log.txt", "a")
-            f.write(str(category_old) + "在" + localtime + "被" + str(obj.owner) + "修改为" + str(category_new) + '\r\n')
+            f.write("Category" + str(category_old) + "在" + localtime + "被" + str(obj.owner) + "修改为" + str(category_new) + '\r\n')
             return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'created_time')
+    list_display = ('name', 'status', 'created_time', 'owner')
     fields = ('name', 'status')
 
     def save_model(self, request, obj, form, change):
         if change:
             obj.owner = request.user
             localtime = time.asctime(time.localtime(time.time()))
-            category_old = self.model.objects.get(pk=obj.pk).name
-            category_new = form.cleaned_data['name']
+            try:
+                tag_old = self.model.objects.get(name=obj.name).name
+            except:
+                tag_old = "👨‍🍳初始信息"
+            tag_new = form.cleaned_data['name']
             f = open("/home/dfl/py-projects/web/django/log.txt", "a")
-            f.write(str(category_old) + "在" + localtime + "被" + str(obj.owner) + "修改为" + str(category_new) + '\r\n')
+            f.write("Tag" + str(tag_old) + "在" + localtime + "被" + str(obj.owner) + "修改为" + str(tag_new) + '\r\n')
             return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status', 'created_time', 'operator']
+    list_display_links = []
+
+    list_filter = ['category']
+    search_fields = ['title', 'category__name']
+
+    actions_on_bottom = True
+
+    fields = [('title', 'category'),
+              'desc', 'status', 'tag', 'content']
+
+    def operator(self, obj):
+        return format_html(
+            '<a href={}>编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+
+    operator.short_description = "操作"
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.owner = request.user
+            localtime = time.asctime(time.localtime(time.time()))
+            try:
+                post_old = self.model.objects.filter(id=obj.pk)
+            except:
+                post_old = "👨‍🍳初始信息"
+            post_new = form.cleaned_data
+            f = open("/home/dfl/py-projects/web/django/log.txt", "a")
+            f.write("Post" + str(post_old) + "在" + localtime + "被" + str(obj.owner) + "修改为" + str(post_new) + '\r\n')
+            return super(PostAdmin, self).save_model(request, obj, form, change)
