c851af52977efb43c1acc68d32bcb7fe3750d80d
xiaolu2333 <2694977226@qq.com>
2021-03-06 14:52:15 +0800

    修改BASE_DIR路径

--- typeidea/settings/base.py
+++ typeidea/settings/base.py
@@ -14,9 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
