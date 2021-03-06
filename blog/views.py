0f69e2e3c01eba785c52d10b160a26e351ef0258
xiaolu2333 <2694977226@qq.com>
2021-03-06 17:57:22 +0800

    获取并展示文章数据
    注意：
    1，views使用到反响查询，对象不要错
    2，模板还使用到外键查询，产生的N+1查询会影响性能

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from .models import Tag, Category, Post
 
 
 # Create your views here.
 def post_list(request, category_id=None, tag_id=None):
-    return render(request, 'list.html', context={'name': 'post_list'})
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            post_list = post_list.filter(category_id=category_id)
+    return render(request, 'list.html', context={'post_list': post_list})
 
 
 def post_detail(request, post_id):
-    return render(request, 'detail.html', context={'name': 'post_detail'})
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'detail.html', context={'post': post})
