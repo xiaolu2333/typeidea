6f46ae55c7edf9c704b2afca1b644f6c6d156cd5
xiaolu2333 <2694977226@qq.com>
2021-03-07 10:59:44 +0800

    修改Post模型，添加‘最热文章’相关字段与方法

--- blog/models.py
+++ blog/models.py
@@ -80,6 +80,8 @@ class Post(models.Model):
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    pv = models.PositiveIntegerField(default=1)
+    uv = models.PositiveIntegerField(default=1)
 
     def colored_status(self):
         if self.status == 0:
@@ -107,7 +109,6 @@ class Post(models.Model):
 
         return post_list, tag
 
-
     @staticmethod
     def get_by_category(category_id):
         try:
@@ -125,12 +126,14 @@ class Post(models.Model):
         queryset = cls.objects.filter(status=cls.STATUS_NORMAL).order_by("-id")
         return queryset
 
+    @classmethod
+    def hot_posts(cls):
+        queryset = cls.objects.filter(status=Post.STATUS_NORMAL).order_by('-pv')
+        return queryset
+
     def __str__(self):
         return self.title
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
         ordering = ['-id']
-
-
-
