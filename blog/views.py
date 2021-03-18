9021778a60711ba08abfb859cff193895eaa80e1
xiaolu2333 <2694977226@qq.com>
2021-03-18 09:17:00 +0800

    使用内置的ListView视图并添加分页器实现文章列表内容的分页展示

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,37 @@
 from django.shortcuts import render
 from blog.models import Tag, Category, Post
 from config.models import SideBar
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 
 # Create your views here.
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
+# def post_list(request, category_id=None, tag_id=None):
+#     tag = None
+#     category = None
+#
+#     if tag_id:
+#         post_list, tag = Post.get_by_tag(tag_id)
+#     elif category_id:
+#         post_list, category = Post.get_by_category(category_id)
+#     else:
+#         post_list = Post.latest_posts()
+#
+#     context = {
+#         'category': category,
+#         'tag': tag,
+#         'post_list': post_list,
+#         'sidebars': SideBar.get_all(),
+#     }
+#     context.update(Category.get_navs(owner=request.user))
+#
+#     return render(request, 'list.html', context=context)
+#
 
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
-
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs(owner=request.user))
-
-    return render(request, 'list.html', context=context)
+class PostListView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 1
+    context_object_name = 'post_list'
+    template_name = 'list.html'
 
 
 class PostDetailView(DetailView):
--- typeidea/urls.py
+++ typeidea/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import post_list, PostDetailView #post_detail
+from blog.views import PostListView, PostDetailView
 from config.views import links
 from .custom_site import custom_site
 
 urlpatterns = [
-    path('', post_list, name="index"),
+    path('', PostListView.as_view(), name="index"),
     path('post/<int:pk>.html', PostDetailView.as_view(), name="post_detail"),
-    path('category/<int:category_id>', post_list, name="category_list"),
-    path('tag/<int:tag_id>', post_list, name="tag_list"),
+    path('category/<int:category_id>', PostListView.as_view(), name="category_list"),
+    path('tag/<int:tag_id>', PostListView.as_view(), name="tag_list"),
     path('links/', links, name="links"),
     path('super_admin/', admin.site.urls, name="super_admin"),
     path('admin/', custom_site.urls, name="admin")
