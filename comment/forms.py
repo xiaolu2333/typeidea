321aa81b36e9b3ff63cce0646d9c740a4bbd974f
xiaolu2333 <2694977226@qq.com>
2021-05-08 00:25:59 +0800

    修改了提交评论表单的样式

--- comment/forms.py
+++ comment/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 from comment.models import Comment
 
@@ -8,30 +9,30 @@ class CommentForm(forms.ModelForm):
         label="昵称",
         max_length=50,
         widget=forms.widgets.Input(
-            attrs={'class': 'form_control', 'style': 'width: 60%;'}
+            attrs={'class': 'form_control', 'style': 'width: 90%;'}
         )
     )
-    email = forms.CharField(
+    email = forms.EmailField(
         label="邮箱",
-        max_length=50,
-        widget=forms.widgets.EmailInput(
-            attrs={'class': 'form_control', 'style': 'width: 60%;'}
-        )
+        max_length=50
     )
     website = forms.CharField(
         label="网站",
         max_length=100,
         widget=forms.widgets.URLInput(
-            attrs={'class': 'form-control', 'style': 'width: 40%;'}
+            attrs={'class': 'form-control', 'style': 'width: 90%;'}
         )
     )
     content = forms.CharField(
         label="内容",
         max_length=500,
         widget=forms.widgets.Textarea(
-            attrs={'class': 'form-control', 'rows': 3, 'clos': 20}
+            attrs={'class': 'form-control', 'rows': 3, 'clos': 10}
         )
     )
+
+    # 这是更灵活的设置CSS属性的办法
+    email.widget.attrs.update({'class': 'form-control', 'style': 'width: 90%;'})
 
     def clean_content(self):
         content = self.cleaned_data.get('content')
