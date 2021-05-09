6e92b1ef14fc7d4b49526e1d5c3fb124ae242d3b
xiaolu2333 <2694977226@qq.com>
2021-05-09 21:58:10 +0800

    使用自定义模板标签抽象出评论模块渲染组件

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,6 @@ from django.views.generic import DetailView, ListView
 from django.shortcuts import get_object_or_404
 from blog.models import Tag, Category, Post
 from config.models import SideBar
-from comment.forms import CommentForm
-from comment.models import Comment
 
 
 # Create your views here.
@@ -72,14 +70,6 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'comment_form': CommentForm,
-            'comment_list': Comment.get_by_target(self.request.path),
-        })
-        return context
 
 
 class SearchView(IndexView):
---
+++ comment/templatetags/commentblocks.py
@@ -0,0 +1,15 @@
+from django import template
+
+from comment.models import Comment
+from comment.forms import CommentForm
+
+register = template.Library()
+
+
+@register.inclusion_tag("comment/blocks/comment_block.html")
+def comment_block(target):
+    return {
+        'target': target,
+        'comment_form': CommentForm(),
+        'comment_list': Comment.get_by_target(target),
+    }
