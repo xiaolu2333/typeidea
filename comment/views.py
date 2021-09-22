982deeea16d04bcac134c76d7b6c325629364839
xiaolu2333 <2694977226@qq.com>
2021-09-23 03:10:14 +0800

    调整评论target的存储内容

--- comment/views.py
+++ comment/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 from comment.forms import CommentForm
+from blog.models import Post
 
 
 # Create your views here.
@@ -12,16 +13,23 @@ class CommentView(TemplateView):
     def post(self, request, *args, **kwargs):
         comment_form = CommentForm(request.POST)
         target = request.POST.get('target')
+        target_type = target.split('/')[1]
+        temp = None
+        if target_type == 'post':
+            target_id = target.split('/')[-1].split('.')[0]
+            temp = Post.objects.get(id=target_id).title # 获取文章标题
+        if target_type == 'links':
+            temp = '友链页面'
 
         if comment_form.is_valid():
             instance = comment_form.save(commit=False)
-            instance.target = target
+            instance.target = temp
             instance.save()
             succeed = True
             return redirect(target)
         else:
             succeed = False
-
+        target = temp
         context = {
             'succeed': succeed,
             'form': comment_form,
